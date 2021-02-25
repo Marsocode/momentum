bf8686094f769b0518cde976d27768bc8215bcc8
Lika <borisovalika2@gmail.com>
2021-02-26 01:25:08 +0300

    fix: citate

--- script/citateScript.js
+++ script/citateScript.js
@@ -4,28 +4,21 @@ const changeCitate = document.querySelector('.btn-changeCitate');
 
 async function getCitate() {
   try {
-
-    const url = 'https://type.fit/api/quotes';
-
+    const url = 'https://favqs.com/api/qotd';
     const res = await fetch(url);
-
     const data  = await res.json();
-    let citate = data[getRandomInt(data.length)];
-    while (data[getRandomInt(data.length)].length >= 80) {
-      citate = data[getRandomInt(data.length)];
-    }
-    citateText.textContent = `"${citate.text}"`;
-    if (citate.author != null) {
-      citateAuthor.textContent = `${citate.author}`;
+    citateText.textContent = `"${data.quote.body}"`;
+    if (data.quote.author !== undefined) {
+      citateAuthor.textContent = `${data.quote.author}`;
     }
   } catch (e) {
     console.log(e.stack);
   }
 }
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * Math.floor(max));
+// }
 
 changeCitate.addEventListener("click", getCitate);
 document.addEventListener('DOMContentLoaded', getCitate);
